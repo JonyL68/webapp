b2dd68b0ce4e59d46d223dca27b70ad0b2d242dc
nimh <e.litvinov@zpep.com.ua>
2023-10-30 17:49:32 +0200

    new class

--- src/main/java/com/webapp/model/ErNew.java
+++ src/main/java/com/webapp/model/ErNew.java
@@ -1,43 +1,55 @@
 package com.webapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import javax.persistence.*;
+import javax.persistence.*;
+import java.util.Objects;
 @Entity
+@Table(name = "er_new", schema = "refrec")
 public class ErNew {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int territory;
+
+    private Integer territory;
     private String account;
     private String adress;
 
-    public int getTerritory() {
+
+    @Id
+    @Column(name = "territory")
+    public Integer getTerritory() {
         return territory;
     }
-
-    public void setTerritory(int territory) {
+    public void setTerritory(Integer territory) {
         this.territory = territory;
     }
 
+    @Basic
+    @Column(name = "account")
     public String getAccount() {
         return account;
     }
-
     public void setAccount(String account) {
         this.account = account;
     }
 
+    @Basic
+    @Column(name = "adress")
     public String getAdress() {
         return adress;
     }
-
     public void setAdress(String adress) {
         this.adress = adress;
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ErNew ernew = (ErNew) o;
+        return Objects.equals(adress, ernew.adress) && Objects.equals(account, ernew.account) && Objects.equals(territory, ernew.territory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(adress, account, territory);
+    }
 }
-
-
