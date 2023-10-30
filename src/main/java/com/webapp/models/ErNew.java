0338fa9e06849fb7a12290cbc4fb0622cedf7e99
nimh <e.litvinov@zpep.com.ua>
2023-10-30 11:58:27 +0200

    getter and setter

--- src/main/java/com/webapp/models/ErNew.java
+++ src/main/java/com/webapp/models/ErNew.java
@@ -1,2 +1,43 @@
-package com.webapp.models;public class ErNew {
+package com.webapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ErNew {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int territory;
+    private String account;
+    private String adress;
+
+    public int getTerritory() {
+        return territory;
+    }
+
+    public void setTerritory(int territory) {
+        this.territory = territory;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+
 }
+
+
